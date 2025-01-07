d5e6540b33febdb88ae58022e5709842cff8fcb9
tim-qtp <2469100031@qq.com>
2025-01-07 16:56:43 +0800

    add: Set不安全

---
+++ src/main/java/com/qtp/set/SetTest.java
@@ -0,0 +1,25 @@
+package com.qtp.set;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class SetTest {
+    public static void main(String[] args) {
+        /**
+         * 1. Set<String> set = Collections.synchronizedSet(new HashSet<>());
+         * 2. Set<String> set = new CopyOnWriteArraySet<>();
+         */
+//        Set<String> set = new HashSet<>();
+//        Set<String> set = new CopyOnWriteArraySet<>();
+        Set<String> set = Collections.synchronizedSet(new HashSet<>());
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0,5));
+                System.out.println(set);
+            },String.valueOf(i)).start();
+        }
+    }
+}
