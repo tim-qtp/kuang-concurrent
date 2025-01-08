f06e6f2b772bf049d90b54dd8dabba1a9375e5e9
tim-qtp <2469100031@qq.com>
2025-01-08 15:09:54 +0800

    add:BlockQueue

---
+++ src/main/java/com/qtp/bq/BlockQueue.java
@@ -0,0 +1,92 @@
+package com.qtp.bq;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class BlockQueue {
+
+    public static void main(String[] args) throws InterruptedException {
+        test4();
+    }
+    /**
+     * 抛出异常
+     */
+    public static void test1(){
+        //需要初始化队列的大小
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<>(3);
+
+        System.out.println(blockingQueue.add("a"));
+        System.out.println(blockingQueue.add("b"));
+        System.out.println(blockingQueue.add("c"));
+        //抛出异常：java.lang.IllegalStateException: Queue full
+//        System.out.println(blockingQueue.add("d"));
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+//        //如果多移除一个
+//        //这也会造成 java.util.NoSuchElementException 抛出异常
+        System.out.println(blockingQueue.remove());
+    }
+    /**
+     * 不抛出异常，有返回值
+     */
+    public static void test2(){
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<>(3);
+        System.out.println(blockingQueue.offer("a"));
+        System.out.println(blockingQueue.offer("b"));
+        System.out.println(blockingQueue.offer("c"));
+        //添加 一个不能添加的元素 使用offer只会返回false 不会抛出异常
+        System.out.println(blockingQueue.offer("d"));
+
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        //弹出 如果没有元素 只会返回null 不会抛出异常
+        System.out.println(blockingQueue.poll());
+    }
+    /**
+     * 等待 一直阻塞
+     */
+    public static void test3() throws InterruptedException {
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<>(3);
+
+        //一直阻塞 不会返回
+        blockingQueue.put("a");
+        blockingQueue.put("b");
+        blockingQueue.put("c");
+
+        //如果队列已经满了， 再进去一个元素  这种情况会一直等待这个队列 什么时候有了位置再进去，程序不会停止
+        blockingQueue.put("d");
+
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        //如果我们再来一个  这种情况也会等待，程序会一直运行 阻塞
+//        System.out.println(blockingQueue.take());
+    }
+
+    /**
+     * 等待 超时阻塞
+     *  这种情况也会等待队列有位置 或者有产品 但是会超时结束
+     */
+    public static void test4() throws InterruptedException {
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<>(3);
+        //存值
+        System.out.println("存值");
+        blockingQueue.offer("a");
+        blockingQueue.offer("b");
+        blockingQueue.offer("c");
+        System.out.println("开始等待");
+        blockingQueue.offer("d",2, TimeUnit.SECONDS);  //超时时间2s 等待如果超过2s就结束等待
+        System.out.println("结束等待");
+        System.out.println("===========取值==================");
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println("开始等待");
+        blockingQueue.poll(2, TimeUnit.SECONDS); //超过两秒 我们就不要等待了
+        System.out.println("结束等待");
+    }
+
+
+}
